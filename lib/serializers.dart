bbac001b025cd76a93dee8b5737136eb73af0ae8
Diego Tori <diegotoridoesandroid@gmail.com>
2021-05-15 02:46:21 -0400

    Added the StandardJsonPlugin to built_value's Serializers.

--- lib/serializers.dart
+++ lib/serializers.dart
@@ -1,9 +1,14 @@
 library serializers;
 
+import 'package:built_value/iso_8601_date_time_serializer.dart';
 import 'package:built_value/serializer.dart';
+import 'package:built_value/standard_json_plugin.dart';
 import 'package:the_infatuation_coding_challenge_flutter/api_service/saved_repo.dart';
 
 part 'serializers.g.dart';
 
 @SerializersFor([SavedRepo])
-final Serializers serializers = _$serializers;
+final Serializers serializers = (_$serializers.toBuilder()
+      ..addPlugin(StandardJsonPlugin())
+      ..add(Iso8601DateTimeSerializer()))
+    .build();
