41a6887edd0e6dda5110fc818186595d7243ab07
Diego Tori <diegotoridoesandroid@gmail.com>
2021-05-16 01:26:43 -0400

    Forgot to add mocks.dart.

---
+++ test/mocks.dart
@@ -0,0 +1,7 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:the_infatuation_coding_challenge_flutter/api_service/reposerver_api_service.dart';
+import 'package:the_infatuation_coding_challenge_flutter/api_service/saved_repo.dart';
+
+class MockRepoServerApiService extends Mock implements RepoServerApiService {}
+
+class MockSavedRepo extends Mock implements SavedRepo {}
